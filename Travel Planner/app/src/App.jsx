3a514432cdc9bf24792d4f0ee67767688c69407f
Salma Mostafa <salma.mostafa5x@gmail.com>
2026-06-12 01:55:56 +0300

    Add files via upload

---
+++ Travel Planner/app/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './App.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Layout from './components/Layout/Layout'
+import Home from './components/Home/Home';
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes'
+import Destinations from './components/Destinations/Destinations'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import Flights from './components/Flights/Flights'
+import NotFound from './components/NotFound/NotFound'
+import TripPlanner from './components/TripPlanner/TripPlanner'
+import Trips from './components/Trips/Trips'
+import Profile from './components/Profile/Profile'
+import Dashboard from './components/Dashboard/Dashboard'
+import SavedFlights from './components/SavedFlights/SavedFlights'
+import PastTrips from './components/PastTrips/PastTrips'
+import UserContextProvider from './context/userContext';
+
+
+function App() {
+
+  let paths = createBrowserRouter([
+    {path:'', element:<Layout/>, children:[
+      {index:true, element: <Home/>},
+      {path:'destinations', element: <ProtectedRoutes> <Destinations/> </ProtectedRoutes>},
+      {path:'tripPlanner', element:<ProtectedRoutes> <TripPlanner/> </ProtectedRoutes>},
+      {path:'flights', element:<ProtectedRoutes> <Flights/> </ProtectedRoutes>},
+      {path:'trips', element:<ProtectedRoutes> <Trips/> </ProtectedRoutes>},
+      {path:'dashboard', element:<ProtectedRoutes> <Dashboard/> </ProtectedRoutes>},
+      {path:'profile', element:<ProtectedRoutes> <Profile/> </ProtectedRoutes>},
+      {path:'savedflights', element: <ProtectedRoutes> <SavedFlights/> </ProtectedRoutes>},
+      {path:'pastTrips', element: <ProtectedRoutes> <PastTrips/> </ProtectedRoutes>},
+      {path:'*', element:<NotFound/>},
+    ]
+
+    },
+          {path:'login', element:<Login/>},
+      {path:'register', element:<Register/>}
+  ])
+
+  return (
+    
+    <UserContextProvider>
+      <ToastContainer position="top-right" />
+      <RouterProvider router = {paths}></RouterProvider>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ Travel Planner/app/src/firebase.jsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeXqzNm0fcnBk1PFT_R1qpwCa1fL276nw",
+  authDomain: "travel-planner-d0050.firebaseapp.com",
+  projectId: "travel-planner-d0050",
+  storageBucket: "travel-planner-d0050.firebasestorage.app",
+  messagingSenderId: "176133657772",
+  appId: "1:176133657772:web:8aa78130d3fc75ac0e7b09"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
