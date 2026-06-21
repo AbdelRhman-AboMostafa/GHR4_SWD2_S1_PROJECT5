2c3e748475170147144b168c5172b2d082eb2db7
Mariam Mamdouh <mariamelzanaty586@gmail.com>
2026-06-21 16:59:15 +0300

    Prevent duplicate flight bookings
    Added a check to prevent booking a flight that has already been booked.

--- Travel Planner/app/src/components/SavedFlights/SavedFlights.jsx
+++ Travel Planner/app/src/components/SavedFlights/SavedFlights.jsx
@@ -101,6 +101,25 @@ export default function SavedFlights() {
       return;
     }
 
+    //check if trip booked before
+    const q = query(
+      collection(db, "bookedTrips"),
+      where("userId", "==", user.uid),
+    );
+
+    const snapshot = await getDocs(q);
+
+    const alreadyBooked = snapshot.docs.some((doc) => {
+      const bookedFlight = doc.data().flightDetails;
+
+      return bookedFlight?.id === flight?.id;
+    });
+
+    if (alreadyBooked) {
+      toast.warning("Trip already booked!");
+      return;
+    }
+
     try {
       await addDoc(collection(db, "bookedTrips"), {
         userId: user.uid,
